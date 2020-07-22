afc7739600c15e262f8a1f4785bcd2eae1d218a1
acquahsamuel <acquahsamuel1996@gmail.com>
2020-07-22 08:47:30 +0000

    help and start  message

--- bot.js
+++ bot.js
@@ -5,13 +5,29 @@ dotenv.config({path: './.env'})
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 
+bot.command([
+    'start', 'help'
+], ctx => {
+    let message = `
+    Hi, Protech Community Media Bot
+    /start , start the bot
+    /help ,  get help
+    
+    Image Request 
+    /ghana - get location of Ghana
+    /singapore - get location of singapore
+    /newyork - get image of New York 
+
+    Get Cities 
+    /cities - get photos of cities 
+    /citieslist - get text file for cities 
+    `;
+    ctx.reply(message);
+})
+
 
 bot.command('newyork', (ctx) => {
-    // Sending image using URL
-    // bot.telegram.sendPhoto(ctx.chat.id, "https://www.google.com/imgres?imgurl=https%3A%2F%2Favatars3.githubusercontent.com%2Fu%2F32119411%3Fs%3D460%26u%3D05936b1345fc29a79fd098577b009846cd8c3565%26v%3D4&imgrefurl=https%3A%2F%2Fgithub.com%2Facquahsamuel&tbnid=OCc-7QIUoRakyM&vet=12ahUKEwjlm-OT7d7qAhWJq6QKHXigAA0QMygAegUIARCRAQ..i&docid=BFEdomCXpabdwM&w=460&h=460&q=acquah%20samuel%20github&ved=2ahUKEwjlm-OT7d7qAhWJq6QKHXigAA0QMygAegUIARCRAQ")
 
-    // sending image using path
-    // bot.telegram.sendPhoto(ctx.chat.id , {source : "res/london.jpg"});
     bot.telegram.sendChatAction(ctx.chat.id, 'upload_photo')
     bot.telegram.sendPhoto(ctx.chat.id, {
         source: 'res/newyork.jpg'
@@ -70,7 +86,7 @@ bot.on('message', async ctx => {
     if (ctx.updateSubTypes[0] === 'document') {
         try {
             let link = await bot.telegram.getFileLink(ctx.message.document.file_id);
-            ctx.reply('You download link : ' + link)
+            ctx.reply('Your download link : ' + link)
         } catch (err) {
             console.log(err)
             ctx.reply(err.description);
@@ -78,7 +94,7 @@ bot.on('message', async ctx => {
     } else if (ctx.updateSubTypes[0] === 'photo') { // console.log(ctx.message.photo);
         try {
             let link = await bot.telegram.getFileLink(ctx.message.photo[0].file_id);
-            ctx.reply('You download link : ' + link)
+            ctx.reply('Your download link : ' + link)
         } catch (err) {
             console.log(err)
             ctx.reply(err.description);
@@ -87,3 +103,10 @@ bot.on('message', async ctx => {
 })
 
 bot.launch()
+
+
+// Sending image using URL (just uncomment the line below)
+// bot.telegram.sendPhoto(ctx.chat.id, "https://www.google.com/imgres?imgurl=https%3A%2F%2Favatars3.githubusercontent.com%2Fu%2F32119411%3Fs%3D460%26u%3D05936b1345fc29a79fd098577b009846cd8c3565%26v%3D4&imgrefurl=https%3A%2F%2Fgithub.com%2Facquahsamuel&tbnid=OCc-7QIUoRakyM&vet=12ahUKEwjlm-OT7d7qAhWJq6QKHXigAA0QMygAegUIARCRAQ..i&docid=BFEdomCXpabdwM&w=460&h=460&q=acquah%20samuel%20github&ved=2ahUKEwjlm-OT7d7qAhWJq6QKHXigAA0QMygAegUIARCRAQ")
+
+// sending image using path
+// bot.telegram.sendPhoto(ctx.chat.id , {source : "res/london.jpg"});
